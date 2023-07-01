03d74cc0440742f4eb2609b0e6c021ee5f86ce14
ckkunc <113551251+ckkunc@users.noreply.github.com>
2023-07-01 17:23:10 -0400

    Print station names and every single item for each station. Only for Chase Breakfast menu.

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ menu_stations = results.find_all("div", class_="menu-station")
 
 for station in menu_stations:
     station_name = station.find("h4", class_="toggle-menu-station-data")
-    print(station_name.text.strip())
-    
+    print(f"\n\n{station_name.text.strip()}:\n")
+    menu_items = station.find("ul")
+    for item in menu_items.find_all("li"):
+        print(item.text.strip())
