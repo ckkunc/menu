b24c99fd7cd155e3ab05b189c550fe7881abed78
ckkunc <113551251+ckkunc@users.noreply.github.com>
2023-08-04 18:34:55 -0400

    Changed default landing page of Django app

--- backend/django/menu/urls.py
+++ backend/django/menu/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from phone_numbers import views
 from django.contrib import admin
 from rest_framework import routers
+from django.views.generic import RedirectView
 
 router = routers.DefaultRouter()
 router.register(r'phone-numbers', views.PhoneNumberViewSet)
@@ -10,6 +11,7 @@ urlpatterns = [
     path('add/', views.add_phone_number, name='add_phone_number'),
     path('list/', views.list_phone_numbers, name='list_phone_numbers'),
     path('delete/<int:phone_number_id>/', views.delete_phone_number, name='delete_phone_number'),
+    path('', RedirectView.as_view(url='/admin/')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
