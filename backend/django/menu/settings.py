b60a6213b1130fcdfe25bbe432842d114909c796
ckkunc <113551251+ckkunc@users.noreply.github.com>
2023-08-06 01:23:10 -0400

    Changed settings.py

--- backend/django/menu/settings.py
+++ backend/django/menu/settings.py
@@ -126,12 +126,14 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 import os
+import django_heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
-django_heroku.settings(locals())
+
